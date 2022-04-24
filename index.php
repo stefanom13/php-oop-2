ec2a46455ca5577e06646ab87d64f5450a025af7
stefanom13 <stefanomancuso13@gmail.com>
2022-04-24 11:12:11 +0200

    inserimento instanza prodotti

--- index.php
+++ index.php
@@ -1,10 +1,45 @@
 <?php
 
 require_once __DIR__ . '/Utente.php';
+require_once __DIR__ . '/models/prodotti.php';
 
-$Utente1 = new Utente('gianni','antonino','via pippo 23');
+$prodotto1 = new prodotto(
+    'Medicinale',
+    'Collare',
+    'Antiparassitario',
+    'Seresto',
+    39.70
+  );
+  $prodotto2 = new prodotto(
+    'Cibo',
+    'Chicken lovely',
+    'Crocchette',
+    'Foodlovedog',
+    22.50
+  );
+  $prodotto3 = new prodotto(
+    'Cuccia',
+    'Cuccia Small',
+    'Cuccia cani taglia s',
+    'CaniAmici',
+    30.00
+  );
+  $prodotto4 = new prodotto(
+    'Cuccia',
+    'Cuccia Large',
+    'Cuccia cani taglia m/l',
+    'CaniAmici',
+    49.90
+  );
+  $prodotto5 = new prodotto(
+    'Cibo',
+    'Fish lovely',
+    'Crocchette',
+    'Foodlovedog',
+    22.00
+  );
+  var_dump($prodotto1, $prodotto2, $prodotto3, $prodotto4,$prodotto5);
 
-echo $Utente1->getNome();
 
 
 ?>
